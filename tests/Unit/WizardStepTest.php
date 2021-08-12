77cfe6eff6e71261fc3aad280a28671f7fe834d9
Erik Wittek <erik@webhippie.de>
2021-08-12 10:28:32 +0200

    Cleanup Tests

--- tests/Unit/WizardStepTest.php
+++ tests/Unit/WizardStepTest.php
@@ -5,45 +5,47 @@ namespace Suenerds\ArcanistRestApiRenderer\Tests\Unit;
 use Mockery as m;
 use Arcanist\AbstractWizard;
 use Illuminate\Http\Request;
+use PHPUnit\Util\Test;
 use Suenerds\ArcanistRestApiRenderer\WizardStep;
 use Suenerds\ArcanistRestApiRenderer\Fields\Field;
 use Suenerds\ArcanistRestApiRenderer\Tests\TestCase;
 
 class WizardStepTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->wizard = m::mock(AbstractWizard::class);
+        $this->wizard->allows('data')->with('editable', null)->andReturn('::editable::');
+        $this->wizard->allows('data')->with('not_editable', null)->andReturn('::not_editable::');
+
+        $this->step = (new TestStep())->init($this->wizard, 1);
+    }
+
     /** @test */
     public function it_doesnt_process_read_only_fields()
     {
-        $step = new TestStep();
+        $request = Request::create(
+            uri: '/url',
+            method: 'POST',
+            content: json_encode([
+                'editable' => '::set_editable::',
+                'not_editable' => '::try_setting_not_editable::',
+            ])
+        );
+        $request->headers->set('content-type', 'application/json');
 
-        // TODO: cleanup
-        $request = Request::create('/url', 'POST', [], [], [], [], [
-            'editable' => 'lululu',
-            'not_editable' => 'lalala',
-        ]);
-      
-
-        $this->assertArrayNotHasKey('not_editable', $step->process($request)->payload());
+        $payload = $this->step->process($request)->payload();
+        $this->assertArrayNotHasKey('not_editable', $payload);
+        $this->assertEquals('::set_editable::', $payload['editable']);
     }
 
     /** @test */
     public function it_transforms_data_for_display()
     {
-        $step = new class extends WizardStep {
-            public function fields(): array
-            {
-                return [
-                    Field::make('editable')->displayUsing(fn ($value) => '::display::'),
-                ];
-            }
-        };
-        
-        $wizard = m::mock(AbstractWizard::class);
-        $wizard->allows('data')->with('editable', null)->andReturn('::editable::');
-
-        $step->init($wizard, 1);
-        
-        $this->assertEquals('::display::', $step->viewData(new Request())['editable']);
+        $this->assertEquals('::EDITABLE::', $this->step->viewData(new Request())['editable']);
+        $this->assertEquals('::not_editable::', $this->step->viewData(new Request())['not_editable']);
     }
 }
 
@@ -52,7 +54,7 @@ class TestStep extends WizardStep
     public function fields(): array
     {
         return [
-            Field::make('editable')->displayUsing(fn ($value) => '::display::'),
+            Field::make('editable')->displayUsing(fn ($value) => strtoupper($value)),
             Field::make('not_editable')->readOnly(),
         ];
     }
